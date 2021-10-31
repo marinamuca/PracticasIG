1fef95cda48812117eefe3c4db908efc9f7703b4
Marina Muñoz <marinamuca01@gmail.com>
2021-10-31 18:46:13 +0100

    before revert 3

--- P3/objetos_B2.cc
+++ P3/objetos_B2.cc
@@ -419,11 +419,15 @@ _cilindro::_cilindro(float radio, int altura, int m){
   _cabeza::_cabeza(){
       _esfera esfera_aux(0.5,20,20);
       esfera = esfera_aux;
+<<<<<<< HEAD
 
       
       
   }
 
+=======
+  }
+>>>>>>> parent of be2f881 (Cara y cuerpo check)
 	void _cabeza::draw(_modo modo, float r1, float g1, float b1, float r2, float g2, float b2, float grosor){
 
     glPushMatrix();
